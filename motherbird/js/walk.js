1cbb308e431e2547cbda231746764b7fb3bce741
Grant <igmrogers@gmail.com>
2026-09-26 11:36:49 -0400

    Launch walks from soundtrack playlists

--- motherbird/js/walk.js
+++ motherbird/js/walk.js
@@ -229,7 +229,7 @@ export async function startWalk({ routeMode = 'tracking' } = {}) {
   beginGpsWatch();
   getCurrentLocation();
   toast('Walk started. Your route is being saved on this device.');
-  window.dispatchEvent(new CustomEvent('walk-started', { detail: { id: state.activeWalk.id } }));
+  window.dispatchEvent(new CustomEvent('walk-started', { detail: { id: state.activeWalk.id, playlistId: document.getElementById('walkSoundtrackSelect')?.value || null } }));
   return state.activeWalk;
 }
 
