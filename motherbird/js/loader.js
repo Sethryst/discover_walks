5f332ce39ae7109323c4a7d4c37f7f83f9cad9bc
Grant <igmrogers@gmail.com>
2026-09-26 11:37:33 -0400

    Connect soundtrack songs to return prompts

--- motherbird/js/loader.js
+++ motherbird/js/loader.js
@@ -25,7 +25,7 @@ import { initGeoCypher } from './geo-cypher.js';
 import { initPwaUpdates } from './pwa-update.js';
 import { initPrimaryShell } from './primary-shell.js';
 import { initStories } from './stories.js';
-import { initRadio } from './radio.js?v=20260926-radio-widget-v1';
+import { initRadio } from './radio.js?v=20260926-radio-return-prompts-v1';
 import { recoverWalkDraft, discardWalk } from './walk.js';
 
 export async function init() {
